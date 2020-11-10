47df9e12dfeb604e11baf0b800d58bf3e70d82b3
junaid <junaid@aardy.com>
2020-11-10 10:39:40 +0000

    framemotion with loading effect

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
+
 import Product from "../components/product/Product";
+import Spinner from "../components/spinner/Spinner";
 import products from "../products";
 
 const SectionWrapper = styled.section`
   padding: 2rem 0;
-
   h1 {
     font-size: ${(props) => props.theme.typos.sizes.XL};
     color: ${(props) => props.theme.colors.dark};
@@ -36,14 +38,32 @@ const SectionWrapper = styled.section`
   }
 `;
 const Home = () => {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(true);
+    }, 2000);
+  }, []);
   return (
     <SectionWrapper className="container">
       <h1>Latest Products</h1>
-      <article>
-        {products.map((product) => {
-          return <Product key={product._id} product={product} />;
-        })}
-      </article>
+      {!show && (
+        <motion.div>
+          <Spinner />
+        </motion.div>
+      )}
+
+      {show && (
+        <motion.article
+          transition={{ duration: 1.5 }}
+          animate={{ opacity: 1 }}
+          style={{ opacity: 0.1 }}
+        >
+          {products.map((product) => {
+            return <Product key={product._id} product={product} />;
+          })}
+        </motion.article>
+      )}
     </SectionWrapper>
   );
 };
